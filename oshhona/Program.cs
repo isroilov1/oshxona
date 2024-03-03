c10fdf519d8ea2aafe5d964091280d9f2afea83d
isroilov1 <isroilov0905@gmail.com>
2024-03-03 10:59:05 +0500

    category ga add qo'shildi!

--- oshhona/Program.cs
+++ oshhona/Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("LocalDB")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("FoodsDB")));
 
 builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
 builder.Services.AddTransient<ICategoryService, CategoryService>();
